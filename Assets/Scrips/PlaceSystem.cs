f35bc29f26e5e5c51effe2e60ef171b2b5f011f0
Simon Du Plooy <simondup9@gmail.com>
2023-08-11 13:23:01 +0200

    SmallChanges

--- Assets/Scrips/PlaceSystem.cs
+++ Assets/Scrips/PlaceSystem.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 
 public class PlaceSystem : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject mouseCursor;
+    [SerializeField] private Grid grid;
     
-    [SerializeField]
-    private InputManager _inputManager;
+    [SerializeField] private GameObject gridHighlight;
+    
+    [SerializeField] private GameObject mouseCursor;
+    
+    [SerializeField] private InputManager _inputManager;
 
     private void Update()
     {
         Vector3 mousePosition = _inputManager.GetSelectedPos();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        Debug.Log("MousePos");
+        Debug.Log(mousePosition);
+        Debug.Log("GridPos");
+        Debug.Log(gridPosition);
+        gridHighlight.transform.position = grid.CellToWorld(gridPosition);
         mouseCursor.transform.position = mousePosition;
     }
 }
